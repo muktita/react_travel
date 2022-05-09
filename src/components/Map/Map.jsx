8cc4aa60099fc7b6540d5a83419bfffbce270412
muktita <muktitkim@csu.fullerton.edu>
2022-05-09 11:57:59 -0700

    Upate Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import {Paper, Typography, useMediaQuery} from "@material-ui/core";
+import LocationOnOutLinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab"
+
+import useStyle from "./style"
 
 const Map = () => {
+    const classes = useStyle();
+    const isMobile = useMediaQuery('(min-width:600px');
+    const coordinates = { lat: 0, lng: 0};
+
     return (
-        <div>
-            <h1> Map </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: "AIzaSyCdqWjS7U7KcaCAlAB9S7Jt1v7kcH9jcNg"}}
+                defaultCenter = {coordinates}
+                center = {coordinates}
+                defaultZoom = {14}
+                margin = {[50,50,50,50]}
+                options = {""}
+                onChange = {""}
+                onChildClick = {""}
+            >
+
+            </GoogleMapReact>
         </div>
-    );
+    )
 }
 export default Map;
